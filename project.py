a412aee2a069c6b1c67a623793646fd069994015
Febry Afriansyah <41625152+hatakecnk@users.noreply.github.com>
2022-12-14 02:05:06 +0700

    Update project.py

--- project.py
+++ project.py
@@ -3,8 +3,7 @@ import sys
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
- 
-# Finding Mean
+
 print("\nNama Anggota Kelompok:\n1. Aldi Winata\n2. Febry Afriansyah\n3. Fadli Ilhami")
 sys.stdout.write(
     "Process %d of %d on %s.\n"
