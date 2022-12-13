8c18f1949041198d39eb485b79c857bc59b4fcd4
Febry Afriansyah <41625152+hatakecnk@users.noreply.github.com>
2022-12-14 02:04:11 +0700

    Update and rename mean.py to project.py

---
+++ project.py
@@ -0,0 +1,12 @@
+from mpi4py import MPI
+import sys
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+data = [37, 48, 52, 59, 68, 74, 80]
+ 
+# Finding Mean
+print("\nNama Anggota Kelompok:\n1. Aldi Winata\n2. Febry Afriansyah\n3. Fadli Ilhami")
+sys.stdout.write(
+    "Process %d of %d on %s.\n"
+    % (rank, size, name))
